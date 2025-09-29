44fdd23e25ebf6b4df87fb7abfde90f8c7ffd82f
Bayron Jojoa <bayron.jojoa.dv@gmail.com>
2025-09-28 20:47:55 -0500

    details

--- init.sh
+++ init.sh
@@ -7,3 +7,20 @@ k3d node create nodo-rock --cluster dev --role agent
 k3d node create nodo-techno --cluster dev --role agent
 k3d node create nodo-salsa --cluster dev --role agent
 
+# 3. crear namespaces
+kubectl create namespace reggaeton
+kubectl create namespace rock
+kubectl create namespace techno
+kubectl create namespace salsa
+
+# 4. aplicar taint a los nodos
+kubectl taint nodes k3d-nodo-reggaeton-0 music=perreo-intenso:NoSchedule 
+kubectl taint nodes k3d-nodo-rock-0 music=guitarra-electrica:NoSchedule 
+kubectl taint nodes k3d-nodo-techno-0 music=bass-boost:NoSchedule 
+kubectl taint nodes k3d-nodo-salsa-0 music=sabor-latino:NoSchedule
+
+# 5 añadir labels para node affinity
+kubectl label nodes k3d-nodo-reggaeton-0 style=urbano
+kubectl label nodes k3d-nodo-rock-0 style=metalero
+kubectl label nodes k3d-nodo-techno-0 style=electronico
+kubectl label nodes k3d-nodo-salsa-0 style=latino
